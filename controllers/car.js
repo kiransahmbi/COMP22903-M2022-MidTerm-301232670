98db28ec7751253a3f909b718f60175126ef0e42
Kiran Sahmbi <kiransahmbi@yahoo.ca>
2022-06-21 00:39:41 -0400

    Update Cars Controllers
    - displayAddPage
    -processAddPage
    -displayEditPage
    -processEditPage
    -performDelete

--- controllers/car.js
+++ controllers/car.js
@@ -4,13 +4,11 @@ let CarModel = require('../models/car');
 // Gets all cars from the Database and renders the page to list them all.
 module.exports.carList = function(req, res, next) {  
     CarModel.find((err, carsList) => {
-        //console.log(carList);
-        if(err)
-        {
+        console.log(carsList);
+        if(err) {
             return console.error(err);
         }
-        else
-        {
+        else {
             res.render('cars/list', {
                 title: 'Cars List', 
                 CarsList: carsList,
@@ -23,21 +21,18 @@ module.exports.carList = function(req, res, next) {
 
 // Gets a car by id and renders the details page.
 module.exports.details = (req, res, next) => {
-    
     let id = req.params.id;
 
     CarModel.findById(id, (err, carToShow) => {
-        if(err)
-        {
+        if(err) {
             console.log(err);
             res.end(err);
         }
-        else
-        {
-            //show the edit view
+        else {
             res.render('cars/details', {
                 title: 'Car Details', 
-                car: carToShow
+                car: carToShow,
+                userName: req.user ? req.user.username : ''
             })
         }
     });
@@ -45,35 +40,93 @@ module.exports.details = (req, res, next) => {
 
 // Renders the Add form using the add_edit.ejs template
 module.exports.displayAddPage = (req, res, next) => {
+    let newItem = CarModel();
     
-    // ADD YOUR CODE HERE        
-
+    res.render('cars/add_edit', {
+        title: 'Add a New Car',
+        car: newItem,
+        userName: req.user ? req.user.username : ''
+    });
 }
 
 // Processes the data submitted from the Add form to create a new car
 module.exports.processAddPage = (req, res, next) => {
 
-    // ADD YOUR CODE HERE
+    let newItem = CarModel({
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price
+    });
 
+    CarModel.create(newItem, (err, item) => {
+        if(err) {
+            res.end(err);
+        }
+        else {
+            res.redirect('/cars/list');
+        }
+    });
 }
 
 // Gets a car by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE
+    let id = req.params.id;
 
+    CarModel.findById(id, (err, itemToEdit) => {
+        if(err) {
+            res.end(err);
+        }
+        else {
+            res.render('cars/add_edit', {
+                title: "Edit Car",
+                car: itemToEdit,
+                userName: req.user ? req.user.username : ''
+            });
+        }
+    });
 }
 
 // Processes the data submitted from the Edit form to update a car
 module.exports.processEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    let updatedItem = CarModel({
+        _id: req.body.id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price
+    });
     
-    // ADD YOUR CODE HERE
-    
+    CarModel.updateOne({_id: id}, updatedItem, (err) => {
+        if (err) {
+            res.end(err);
+        }
+        else {
+            res.redirect('/cars/list')
+        }
+    });
 }
 
 // Deletes a car based on its id.
 module.exports.performDelete = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE
+    let id = req.params.id;
 
+    CarModel.remove({_id: id}, (err) => {
+        if(err) {
+            res.end(err);
+        }
+        else {
+            res.redirect('/cars/list');
+        }
+    });
 }
